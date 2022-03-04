35c1bf2f2e56f551c8051398ef32200422b505fd
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 00:20:48 -0500

    restas

---
+++ Operadores Básicos/Ejemplos de Restas/Restas_3.py
@@ -0,0 +1,5 @@
+a = int(input("Ingrese Valor: "))
+b = int(input("Ingrese Valor: "))
+
+operacion = (a - b) - (b + b) + a
+print("El resultado de la rests es: ", '(',a,'-',b,')','-','(',b,'+',b,')','+',a,'=',operacion )
