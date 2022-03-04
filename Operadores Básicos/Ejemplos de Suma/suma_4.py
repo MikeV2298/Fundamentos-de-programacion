5e85f83ec1e0a0fd87a046baa1d334d213b6c031
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 00:02:52 -0500

    sumas

---
+++ Operadores Básicos/Ejemplos de Suma/suma_4.py
@@ -0,0 +1,9 @@
+
+p = int(input("Ingese un valor entero: "))
+q = float(input("Ingese un valor decimal: "))
+r = float(input("Ingese un valor decimal: "))
+
+suma1 = (q + r) + (p + q)
+suma2 = q + r + q
+
+print("Resultaddo de la operación: ", sum([suma1, suma2]))
