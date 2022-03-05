169ec7ec242bc765f958d1c1bcb4d056ac53808f
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:52:54 -0500

    while

---
+++ Tomando decisiones/ejer_while.py
@@ -0,0 +1,23 @@
+
+# Ejempolo 1
+
+num=-1
+while not(num>=10 and num <=20 and num%2==0):
+    num=int(input("Ingrese nuevamente un numero:"))
+print("....")
+
+#EJEMPLO 2
+num = 2
+
+while num < 9 or num > 19:
+    num = int(input('ingresar numero positivo: '))
+
+print('.....')
+
+#EJEMPLO 3
+numero = 0
+print('Tabla del 3')
+while numero <= 10:
+    print(f'{numero * 3}')
+    numero += 1
+print('Fin')
