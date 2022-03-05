e4e24d5a140fc5cfe155d51da11eda4dd896f396
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:38:55 -0500

    decimales

---
+++ Tipos de datos en Python/float_ejercicios.py
@@ -0,0 +1,22 @@
+#EJEMPLO 1
+z = 8.2
+c = 9.9
+h = 6.98
+multi = z*c*h
+print(multi)
+
+#EJEMPLO 2
+x = 1.10
+y = 10.0
+z = -40.45
+print ("x =",x, "y =",y,"z =", z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#EJEMPLO 3
+y = 23.54
+z = 24.4
+operacion = (y + z) * y
+print('El resultado de la operacion es: ',operacion)
+print(type(operacion))
