3eacf147a15129f32a7650d34d8132e49a25909f
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:37:04 -0500

    diccionario

---
+++ Tipos de datos en Python/ejemplos_dictionary.py
@@ -0,0 +1,20 @@
+# Ejercicio 1
+prueba_deportiva={'Jorge':550,'Diana':100,'Rodrigo':690,'Julia':700,'Li':600}
+nota=prueba_deportiva
+print(nota['Rodrigo'])
+
+#EJEMPLO 2
+dicti = {
+"marca": "Lotus",
+"modelo": "Evora",
+"anho": 2010
+}
+print(dicti)
+print(type(dicti))
+
+#EJEMPLO 3
+masas_mol = {'H20': 18, 'CO2': 44, 'H2': 2}
+print (masas_mol['CO2'])
+print (masas_mol['H20'])
+print (masas_mol['H2'])
+print(type(masas_mol))
