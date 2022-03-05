4b8c1e95b85354ee3c7babf573f314da88d6b774
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:46:14 -0500

    tuple

---
+++ Tipos de datos en Python/tuplas.py
@@ -0,0 +1,20 @@
+# Ejemplo 1
+vocales = 'a', 'e', 'i', 'o', 'u'
+primer = vocales[2:3]  # Elementos desde el índice 2 hasta el índice 3-1
+print(primer)
+segundo = vocales[2:4]  # Elementos desde el 2 hasta el índice 4-1
+print(segundo)
+todos = vocales[:]  # Todos los elementos
+print(todos)
+
+#EJEMPLO 2
+datos = 9,"Mike","Alex",22
+
+print(datos[1])
+print(type(datos))
+
+#EJEMPLO 3
+datos = 4,7,2003,'J','S','A'
+
+print("List de Tuplas: ",datos)
+print(type(datos))
