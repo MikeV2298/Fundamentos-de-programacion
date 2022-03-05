df1d3de00cd107edddc6009ee89f6a357935101d
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:41:25 -0500

    lista

---
+++ Tipos de datos en Python/list.py
@@ -0,0 +1,18 @@
+#EJEMPLO 1
+numeros = [15, 25, 35]
+numeros *= 25
+
+print(numeros)
+print(type(numeros))
+
+#EJEMPLO 2
+vocales = ['o', 'o', 'o', 'o', 'u']
+vocales[0] = 'a'
+
+print(vocales)
+print(type(vocales))
+
+#EJEMPLO 3
+lista3 = ['Arquero', 'Carrilero', 'Portero', 'Lateral']
+desmarcaciones= lista3[1]
+print(desmarcaciones)
