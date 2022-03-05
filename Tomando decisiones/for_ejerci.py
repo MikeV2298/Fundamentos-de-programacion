c035699d344c7b4a2d9ee43052d34274298df87d
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:54:47 -0500

    for

---
+++ Tomando decisiones/for_ejerci.py
@@ -0,0 +1,16 @@
+
+#EJEMPLO 1
+n = int(input('Ingrese un numero:'))
+
+
+for i in range(1,13):
+    print(f"{i} x {n} = {i*n}")
+    
+#EJEMPLO 2
+nums = [4, 78, 9, 84]
+for n in nums:
+    print(n)
+
+#EJEMPLO 3
+for num in range(0, 11, 2):
+    print(num)
