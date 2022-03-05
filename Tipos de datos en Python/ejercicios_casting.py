54138af838ec4851b16d196e38a41a6742a5dca4
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:27:19 -0500

    tutorial

---
+++ Tipos de datos en Python/ejercicios_casting.py
@@ -0,0 +1,21 @@
+#EJEMPLO 1
+a = 10
+b = 15.4
+
+x = a + b
+print ("x =",x)
+print(type(x))
+
+#EJEMPLO 2
+vrbl1 = "88"
+vrbl2= 5
+result = int(vrbl1) + vrbl2 
+print('la suma es = ',result)
+
+#EJEMPLO 3
+num1 = 22
+num2 = 5.5
+num3 = str(num1 + num2)
+
+print('El resultado de la operacion es: ',num3)
+print(type(num3))
