72d05c746e67445bfc2a6d9ed3ca49e3c0fe2c14
MikeV2298 <99736096+MikeV2298@users.noreply.github.com>
2022-03-04 22:43:58 -0500

    string

---
+++ Tipos de datos en Python/string.py
@@ -0,0 +1,14 @@
+# Ejemplo 1
+x = 'Hola Mundo'
+
+print(x)
+
+# Ejemplo 2
+name = str(input('ingresar primer nombre: '))
+print(name)
+
+#EJEMPLO 3
+x = "sabado"
+y = "domingo"
+
+print("los fines de semana son:", x,"y", y)
